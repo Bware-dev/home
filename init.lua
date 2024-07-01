2659930c84e223e96b74b3f1e13390cc270e0609
Grey <127057031+grey-cmd@users.noreply.github.com>
2024-07-02 01:40:59 +1000

    Add init.lua
    ez

---
+++ init.lua
@@ -0,0 +1,46 @@
+vim.cmd("set noexpandtab")
+vim.cmd("set tabstop=5")
+vim.cmd("set softtabstop=5")
+vim.cmd("set shiftwidth=5")
+vim.cmd("colo retrobox")
+
+vim.g.mapleader = " "
+
+vim.opt.scrolloff = 13
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+vim.keymap.set('n', '<leader>e', ':Explore<CR>', { noremap = true, silent = true })
+
+--------------------------------
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+---------------------------------
+
+local plugins = {
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }
+	}
+}
+
+local opts = {}
+
+---------------------------------
+
+require("lazy").setup(plugins, opts)
+
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<leader>p', builtin.find_files, {})
+vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
